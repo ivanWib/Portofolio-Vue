672981060eb3dda9a5c3c65ac217d1baff2f4318
Ivan Wibowo <christian.ivan@student.umn.ac.id>
2023-11-26 21:39:40 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 className="text-xl underline ">test</h1>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <h1>hello</h1>
+</template>
